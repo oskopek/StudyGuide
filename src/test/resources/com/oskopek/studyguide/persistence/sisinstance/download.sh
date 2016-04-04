a0e9d88e55f5e9c4d7a5bf3b60de32e20f0b16ed
Ondrej Skopek <skopekondrej@gmail.com>
2016-04-04 15:41:43 +0200

    Finished up download sis script

--- src/test/resources/com/oskopek/studyguide/persistence/sisinstance/download.sh
+++ src/test/resources/com/oskopek/studyguide/persistence/sisinstance/download.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 for id in `cat predmety.txt`; do
-    wget 'https://is.cuni.cz/studium/predmety/index.php?do=predmet&kod='"$id"
+    echo "Downloading $id"
+    curl -sS 'https://is.cuni.cz/studium/predmety/index.php?do=predmet&kod='"$id" -o 'index.php?do=predmet&kod='"$id" &
+done
+sleep 1s
+count=`ps aux | grep curl | grep -v grep | wc -l`
+while [ $count -ne 0 ]; do
+    echo "Waiting to finish downloading... $count files"
+    count=`ps aux | grep curl | grep -v grep | wc -l`
+    sleep 1s
 done
