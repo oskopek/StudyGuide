1ce1a4c1ef752df490fb4e8366541fd5309a23cb
Ondrej Skopek <skopekondrej@gmail.com>
2016-05-21 01:23:03 +0200

    Fixed font issue

--- src/main/resources/com/oskopek/studyguide/controller/build.sh
+++ src/main/resources/com/oskopek/studyguide/controller/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 asciidoctor *.adoc
+for i in `ls *.html`; do
+    tmp=`mktemp`
+    grep -Ev '<link rel=\"stylesheet\".*' "$i" > "$tmp" # the font stylesheet doesn't play well with JavaFX's WebView
+    mv "$tmp" "$i"
+done
